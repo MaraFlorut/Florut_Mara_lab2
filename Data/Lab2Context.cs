3d7cfb9b141ea0f7840b0f050c51768e41411525
floru <floru@DESKTOP-O6GH29D>
2024-10-20 19:16:59 +0300

    Add project files.

---
+++ Data/Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab2.Models;
+
+namespace Lab2.Data
+{
+    public class Lab2Context : DbContext
+    {
+        public Lab2Context (DbContextOptions<Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab2.Models
+{
+    public class Author
+    {
+        public int AuthorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
