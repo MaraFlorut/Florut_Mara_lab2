e1fc6fe7dc24760ebda2f747375b7f7896b6d16b
MARA-ȘTEFANIA FLORUȚ <mara.florut@stud.ubbcluj.ro>
2024-10-30 19:22:52 +0200

    Laborator 4

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Lab2.Data;
 using Lab2.Models;
+using Lab2.Models.ViewModels;
 
 namespace Lab2.Pages.Categories
 {
@@ -20,10 +21,28 @@ namespace Lab2.Pages.Categories
         }
 
         public IList<Category> Category { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public CategoriesIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            //Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoriesIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(i => i.BookCategories)
+                .ThenInclude(c => c.Book)
+                .ThenInclude(a => a.Author)
+                .OrderBy(i => i.CategoryName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                    .Where(i => i.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories
+                    .Select(bc => bc.Book);
+            }
         }
     }
 }
